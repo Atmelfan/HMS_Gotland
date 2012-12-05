a97b3cd9ef2fe9afba1ff373286e55ed23318268
Atmelfan <gustavp@hotmail.se>
2012-12-05 22:51:07 +0100

    First commit

---
+++ HMS_Gotland/src/hms_gotland_core/HMS_Gotland.java
@@ -0,0 +1,33 @@
+package hms_gotland_core;
+
+public class HMS_Gotland
+{
+	public static HMS_Gotland game;
+	private boolean exit;
+	
+	private void run()
+	{
+		init();
+		
+		while(!exit)
+		{
+			//Game loop
+		}
+		
+	}
+
+	private void init()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		game = new HMS_Gotland();
+		game.run();
+	}
+}
